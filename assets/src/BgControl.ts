efb761fc236688e94fe8ff40340baa4e9a05760e
alysee <18682330079@163.com>
2023-04-11 23:26:44 +0800

    feat: uuu

--- assets/src/BgControl.ts
+++ assets/src/BgControl.ts
@@ -3,12 +3,24 @@ const { ccclass, property } = _decorator
 
 @ccclass('BgControl')
 export class BgControl extends Component {
+  @property(Node)
+  bg1: Node = null
+  @property(Node)
+  bg2: Node = null
+
   start() {}
 
   update(deltaTime: number) {
-    for (const child of this.node.children) {
-      const curPos = child.getPosition()
-      child.setPosition(new Vec3(0, (curPos.y -= 50 * deltaTime)))
+    const bg1y = this.bg1.getPosition().y - 200 * deltaTime
+    this.bg1.setPosition(new Vec3(0, bg1y))
+    const bg2y = this.bg2.getPosition().y - 200 * deltaTime
+    this.bg2.setPosition(new Vec3(0, bg2y))
+
+    if (bg1y <= -800) {
+      this.bg1.setPosition(new Vec3(0, 800 + bg2y))
+    }
+    if (bg2y <= -800) {
+      this.bg2.setPosition(new Vec3(0, 800 + bg1y))
     }
   }
 }
